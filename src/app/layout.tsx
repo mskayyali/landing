879a2e4c54ad8bfecf9ad4a0635c1fe7b9ed155f
mskayyali <salehmeka@gmail.com>
2025-02-27 09:22:04 +0000

    add: Microsoft Clarity tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from 'next/font/google';
 import "./globals.css";
+import Script from 'next/script';
 
 const jetBrainsMono = JetBrains_Mono({
   subsets: ['latin'],
@@ -41,6 +42,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={jetBrainsMono.variable}>
+      <Script
+        id="microsoft-clarity"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "hr47ief3ei");
+          `
+        }}
+      />
       <body suppressHydrationWarning>
         {children}
       </body>
